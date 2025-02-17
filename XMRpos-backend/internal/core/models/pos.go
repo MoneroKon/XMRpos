2c1ba0734659134e89b5d6ae3d1d89ef890d411d
LeonHellqvist <leon@hellqvist.dev>
2025-02-17 17:15:03 +0100

    Change default PasswordVersion to 1

--- XMRpos-backend/internal/core/models/pos.go
+++ XMRpos-backend/internal/core/models/pos.go
@@ -8,7 +8,7 @@ type POS struct {
 	gorm.Model
 	Name               string        `gorm:"unique;not null"`
 	PasswordHash       string        `gorm:"not null"`
-	PasswordVersion    uint32        `gorm:"not null"`
+	PasswordVersion    uint32        `gorm:"not null;default:1"`
 	VendorID           uint          `gorm:"not null"` // Foreign key field
 	Vendor             Vendor        `gorm:"foreignKey:VendorID"`
 	DeviceTransactions []Transaction `gorm:"foreignKey:POSID"`
